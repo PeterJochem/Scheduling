459476f692f3cf686030604ea68eb810e2e4d6d2
Peter Jochem <peterpennrichjochem@gmail.com>
2020-11-03 10:17:44 -0600

    I added some command line arguments for specifying the order

--- README.md
+++ README.md
@@ -4,16 +4,23 @@
 ```make```
 
 # How To Run the Scheduler 
-Describe how to get a schedule and plot/display it
-
+Follow the steps above to compile and then from the build directory, run ```./createSchedule arg1, ..., arg5```, where arg1 to arg5 are the names of the recipes in the order. For example, you might run ```./createSchedule asparagus_soup.txt carrot_soup.txt chili_con_carne.txt chocolate_ice_cream.txt mushroom_risotto.txt```. This will create a schedule and write it to a csv file. To then create the gant chart, run ```cd ../src``` and then ```python3 plotSchedule.py```. The schedule is written to src/schedule.png. To view it, run vlc ```schedule.png```. To view all available recipes, run ```./createSchedule help```. This will show you a list of available recipes.
 
 # Toy Dataset
-I created a set of text files that describe recipes. They are at allRecipes/<recipe_name>.txt
+I created a set of text files that describe recipes. The text files are at allRecipes/<recipe_name>.txt
+
+# Results 
+Show a schedule
 
 
-# gTesting Tests
-test/runUnitTests has a set of tests for the repo. To run the tests, ```cd build``` <br />
+# Unit Testing
+I used gtest to create unit tests. test/runUnitTests has a set of tests for creating schedules. To run the tests, ```cd build``` <br />
 ```cmake ..``` <br />
 ```make``` <br />
 ```./runUnitTests```
 
+# Dependencies
+matplotlib, (```pip install matplotlib```)
+C++ 14 <br />
+Python3 <br />
+CMake <br />
--- src/createSchedule.cpp
+++ src/createSchedule.cpp
@@ -1,14 +1,60 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include "../include/order.hpp"
+using namespace std;
 
-int main() {
-	using namespace std;
+tuple<vector<string>, bool> parseInputs(int numUserRecipes, char* userRecipes[]) { 
+	
+	vector<string> legal_recipes = {"asparagus_soup.txt", "carrot_soup.txt", "chili_con_carne.txt", "chocolate_ice_cream.txt", "mushroom_risotto.txt", 
+					"sabayon.txt", "strawberry_ice_cream.txt"};
+	vector<string> legalUserRecipes;
+
+	if (userRecipes[0] == string("help")) {
+		cout << "The available recipes are below \n";	
+		for (auto itr = legal_recipes.begin(); itr < legal_recipes.end(); itr++) {
+			cout << *itr << "\n";
+		}
+
+		cout << endl;
+		return {vector<string>(), false};
+	}	
+
+	bool allRecipesLegal = true; 	
+	for (int i = 0; i < numUserRecipes; i++) {
+		
+		if (find(legal_recipes.begin(), legal_recipes.end(), userRecipes[i]) == legal_recipes.end()) { 
+			cout << "You entered a recipe (" << userRecipes[i] << ") not in our menu. ";
+		        cout << "To view all available recipes. Run ./createSchedule help" << endl;	
+			allRecipesLegal = false;
+		}
+		else {
+			legalUserRecipes.push_back(userRecipes[i]);
+		}
+	}	
+
+	return {legalUserRecipes, allRecipesLegal};	
+}
+
+
+int main(int argc, char* argv[]) {
+
+	if (argc > 6) {
+		cout << "You entered too many recipes for the system. Please enter 1-5 recipes" << endl;
+		return -1;
+	}
+	if (argc == 1) {
+		cout << "You entered too few recipes for the system. Please enter 1-5 recipes" << endl;
+                return -1;
+	}
+
+	auto[recipe_ids, allRecipesLegal] = parseInputs(argc - 1, argv + 1);
+	if (!allRecipesLegal) {
+		return -1;
+	}	
 
 	cout << "Starting Schedule Creation" << endl;
-		
-	vector<string> recipe_ids = {"chocolate_ice_cream.txt", "chili_con_carne.txt", "asparagus_soup.txt"};
-        order newOrder = order(recipe_ids);
+	order newOrder = order(recipe_ids);
 	
 	string fileName = "schedule.csv";
 	newOrder.createSchedule(fileName);
